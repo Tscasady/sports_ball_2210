64fe4ad3ffdf3b058425205fc43dff61834dd87a
Thomas Casady <tscasady@gmail.com>
2022-10-24 17:24:50 -0500

    test: Add tests for players_by_last_name and average_cost_of_player methods for Team.

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -117,6 +117,43 @@ end
       expect(team.details).to be == {"total_value" => 85200000, "player_count" => 4}
     end
   end
+
+  describe '#average_cost_of_player' do
+    it 'returns a striing representing the average cost of the players on the team' do
+      team = Team.new('Dodgers', "Los Angeles")
+
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.average_cost_of_player).to eq "$21,300,000"
+    end
+  end
+
+  describe '#players_by_last_name' do
+    it 'returns a string of player last names sorted by alaphabetical order.' do
+      team = Team.new('Dodgers', "Los Angeles")
+
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.players_by_last_name).to eq "DeNunez, McClennan, Palledorous, Porter"
+    end
+  end
+
 end
 
 
