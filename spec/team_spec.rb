1a495dd1338ec6f29a5e032d7c0f46eb1cdd10af
Thomas Casady <tscasady@gmail.com>
2022-10-24 16:48:57 -0500

    test: Add tests for name, city, roster, player_count, and add_player methods for Team.d

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,1 +1,39 @@
 require './lib/team'
+
+describe Team do
+  describe '#initialize' do
+    it 'has a name' do
+      team = Team.new('Dodgers', "Los Angeles")
+
+      expect(team.name).to eq 'Dodgers'
+    end
+
+    it 'has a city' do
+      team = Team.new('Dodgers', "Los Angeles")
+
+      expect(team.city).to eq 'Los Angeles'
+    end
+
+    it 'has an empty roster array by default' do
+      team = Team.new('Dodgers', "Los Angeles")
+
+      expect(team.roster).to eq []
+    end
+  end
+
+  describe '#player_count' do
+    it "returns the total number of players in the team's roster" do
+    end
+  end
+
+  describe '#add_player' do
+    it "can add a player to the team's roster" do
+    end
+  end
+end
+
+
+
+
+
+
