677dd5b63904845acb17c316f638048342542be6
Thomas Casady <tscasady@gmail.com>
2022-10-24 16:54:35 -0500

    feat: Add name, city, roster, player_count, and add_player methods and update tests.

--- lib/team.rb
+++ lib/team.rb
@@ -1,2 +1,18 @@
 class Team
+
+  attr_reader :name, :city, :roster
+
+  def initialize(name, city)
+    @name = name
+    @city = city
+    @roster = []
+  end
+
+  def player_count
+    @roster.length
+  end
+
+  def add_player(player)
+    @roster << player
+  end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -21,13 +21,28 @@ describe Team do
     end
   end
 
+  describe '#add_player' do
+  it "can add a player to the team's roster" do
+    team = Team.new('Dodgers', "Los Angeles")
+
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    team.add_player(player_1)
+
+    expect(team.roster[0]).to eq player_1
+  end
+end
+  
   describe '#player_count' do
     it "returns the total number of players in the team's roster" do
-    end
-  end
+      team = Team.new('Dodgers', "Los Angeles")
 
-  describe '#add_player' do
-    it "can add a player to the team's roster" do
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+
+      expect(team.player_count).to eq 2
     end
   end
 end
